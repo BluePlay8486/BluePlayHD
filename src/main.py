d86b48ffb1232d27380a20ca1ba640ed67598959
BluePlay <denisbarbosadossantosdbds5545@gmail.com>
2025-04-10 20:21:22 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -6,23 +6,18 @@ from config import OUTPUT_XML_PATH
 
 def main():
     print("Iniciando geração do XML personalizado...")
+    
+    print("Baixando lista M3U...")
+    m3u_content = baixar_m3u()
+    print("Download da M3U concluído.")
 
-    try:
-        print("Baixando lista M3U...")
-        m3u_content = baixar_m3u()
-        print("Download da M3U concluído.")
+    print("Processando lista e filtrando grupos desejados...")
+    canais_por_grupo = parse_m3u(m3u_content)
+    print(f"Total de grupos encontrados: {len(canais_por_grupo)}")
 
-        print("Processando lista e filtrando grupos desejados...")
-        canais_por_grupo = parse_m3u(m3u_content)
-        print(f"Total de grupos encontrados: {len(canais_por_grupo)}")
-
-        print("Gerando arquivo XML final...")
-        gerar_xml(canais_por_grupo, OUTPUT_XML_PATH)
-        print(f"XML gerado com sucesso em: {OUTPUT_XML_PATH}")
-
-    except Exception as e:
-        print(f"Erro ao gerar XML: {e}")
-        exit(1)
+    print("Gerando arquivo XML final...")
+    gerar_xml(canais_por_grupo, OUTPUT_XML_PATH)
+    print(f"XML gerado com sucesso em: {OUTPUT_XML_PATH}")
 
 
 if __name__ == "__main__":
